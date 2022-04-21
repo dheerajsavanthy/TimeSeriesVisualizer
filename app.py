f6d2f60ecb20f7f788e63ba54c40c1191e183269
dheerajsavanthy <91928767+dheerajsavanthy@users.noreply.github.com>
2022-04-20 22:57:49 -0400

    first commit

--- app.py
+++ app.py
@@ -1,152 +1,45 @@
 import numpy as np
 import pandas as pd
-import spotipy
 import os
-#import pickle
-from flask import Flask, request
+import pickle
+from flask import Flask, request, url_for, session, flash, Response
 from flask import Flask, request, jsonify, render_template
+import json
+import plotly
+import plotly.express as px
+from datetime import datetime
+import io
+import statsmodels.api as sm
+from scipy import stats
+
+dfi = pd.read_csv("bitcoin.csv")
+dfi.Timestamp = pd.to_datetime(dfi.Timestamp, unit='s')
+dfi.index = dfi.Timestamp
+df_day= dfi.resample('D').mean()
+df_day = df_day.iloc[1200:,:]
+sm.tsa.seasonal_decompose(df_day.Weighted_Price)
+df_day['Weighted_Price_box'], lmbda = stats.boxcox(df_day.Weighted_Price)
 
 
-spotify_data = pd.read_csv('data.csv')
-genre_data = pd.read_csv('data_by_genres.csv')
-data_by_year = pd.read_csv('data_by_year.csv')
+bestmodel = pickle.load( open( "ARIMA_best_model.pkl", "rb" ) )
 
-from sklearn.cluster import KMeans
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
-
-cluster_pipeline = Pipeline([('scaler', StandardScaler()), ('kmeans', KMeans(n_clusters=10, n_jobs=-1))])
-X = genre_data.select_dtypes(np.number)
-cluster_pipeline.fit(X)
-genre_data['cluster'] = cluster_pipeline.predict(X)
-
-song_cluster_pipeline = Pipeline([('scaler', StandardScaler()), 
-                                  ('kmeans', KMeans(n_clusters=20, 
-                                   verbose=2, n_jobs=4))],verbose=False)
-X = spotify_data.select_dtypes(np.number)
-number_cols = list(X.columns)
-song_cluster_pipeline.fit(X)
-song_cluster_labels = song_cluster_pipeline.predict(X)
-spotify_data['cluster_label'] = song_cluster_labels
-
-
-from spotipy.oauth2 import SpotifyClientCredentials
-from collections import defaultdict
-
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="e265493d7f8d4e559fb03bc3f59c2ddc",
-                                                           client_secret="a01b5931546d404d97b6f633ffbcd56f"))
-
-
-def find_song(name, year):
-  
-    """
-    This function returns a dataframe with data for a song given the name and release year.
-    The function uses Spotipy to fetch audio features and metadata for the specified song.
+def prepare_input(start_date, end_date):
+    core_date = '2011-12-31'
+    startdate = datetime.strptime(start_date, '%Y-%m-%d').date()
+    enddate = datetime.strptime(end_date, '%Y-%m-%d').date()
+    coredate = datetime.strptime(core_date, '%Y-%m-%d').date()
+    startrange = startdate-coredate
+    endrange = enddate-coredate
+    starter = startrange.days
+    ender = endrange.days
     
-    """
-    
-    song_data = defaultdict()
-    results = sp.search(q= 'track: {} year: {}'.format(name,
-                                                       year), limit=1)
-    if results['tracks']['items'] == []:
-        return None
-    
-    results = results['tracks']['items'][0]
+    return starter, ender
 
-    track_id = results['id']
-    audio_features = sp.audio_features(track_id)[0]
-    
-    song_data['name'] = [name]
-    song_data['year'] = [year]
-    song_data['explicit'] = [int(results['explicit'])]
-    song_data['duration_ms'] = [results['duration_ms']]
-    song_data['popularity'] = [results['popularity']]
-    
-    for key, value in audio_features.items():
-        song_data[key] = value
-    
-    return pd.DataFrame(song_data)
-
-from collections import defaultdict
-from scipy.spatial.distance import cdist
-import difflib
-
-number_cols = ['valence', 'year', 'acousticness', 'danceability', 'duration_ms', 'energy', 'explicit',
- 'instrumentalness', 'key', 'liveness', 'loudness', 'mode', 'popularity', 'speechiness', 'tempo']
-
-def get_song_data(song, spotify_data):
-    
-    """
-    Gets the song data for a specific song. The song argument takes the form of a dictionary with 
-    key-value pairs for the name and release year of the song.
-    """
-    
-    try:
-        song_data = spotify_data[(spotify_data['name'] == song['name']) 
-                                & (spotify_data['year'] == song['year'])].iloc[0]
-        return song_data
-    
-    except IndexError:
-        return find_song(song['name'], song['year'])
-        
-
-def get_mean_vector(song_list, spotify_data):
-  
-    """
-    Gets the mean vector for a list of songs.
-    """
-    
-    song_vectors = []
-    
-    for song in song_list:
-        song_data = get_song_data(song, spotify_data)
-        if song_data is None:
-            print('Warning: {} does not exist in Spotify or in database'.format(song['name']))
-            continue
-        song_vector = song_data[number_cols].values
-        song_vectors.append(song_vector)  
-    
-    song_matrix = np.array(list(song_vectors))
-    return np.mean(song_matrix, axis=0)
-
-def flatten_dict_list(dict_list):
-   
-    """
-    Utility function for flattening a list of dictionaries.
-    """
-    
-    flattened_dict = defaultdict()
-    for key in dict_list[0].keys():
-        flattened_dict[key] = []
-    
-    for dictionary in dict_list:
-        for key, value in dictionary.items():
-            flattened_dict[key].append(value)
-            
-    return flattened_dict
-        
-
-def recommend_songs(song_list, spotify_data, n_songs=10):
-  
-    """
-    Recommends songs based on a list of previous songs that a user has listened to.
-    """
-    
-    metadata_cols = ['name', 'year', 'artists']
-    song_dict = flatten_dict_list(song_list)
-    
-    song_center = get_mean_vector(song_list, spotify_data)
-    scaler = song_cluster_pipeline.steps[0][1]
-    scaled_data = scaler.transform(spotify_data[number_cols])
-    scaled_song_center = scaler.transform(song_center.reshape(1, -1))
-    distances = cdist(scaled_song_center, scaled_data, 'cosine')
-    index = list(np.argsort(distances)[:, :n_songs][0])
-    
-    rec_songs = spotify_data.iloc[index]
-    rec_songs = rec_songs[~rec_songs['name'].isin(song_dict['name'])]
-    return rec_songs[metadata_cols].to_dict(orient='records')
-
-
+def invboxcox(y,lmbda):
+    if lmbda == 0:
+        return(np.exp(y))
+    else:
+        return(np.exp(np.log(lmbda*y+1)/lmbda))
 
 app=Flask(__name__)
 
@@ -161,24 +54,19 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    features = [x for x in request.form.values()]
-    list = []
-    list.append('name')
-    list.append(features[0])
-    list.append('year')
-    num = int(features[1])
-    list.append(num)
-    dicto = {list[i]:list[i+1] for i in range(0, len(list), 2)}
-    dic = []
-    dic.append(dicto)
-        
-    rec_songs = recommend_songs(dic, spotify_data)
-    output = ' '
-    for i in rec_songs:
-        for key, value in i.items():
-            output = output + '\n' + key + ': ' + str(value) + ', ' 
-    
-    return render_template('index.html', prediction_text='Here are some recommendations for you: {}'.format(output))
+    dates = [x for x in request.form.values()]
+    start_date = dates[0]
+    end_date = dates[1]
+    a, z = prepare_input(start_date, end_date)
+    predict = invboxcox(bestmodel.predict(start=a, end=z), lmbda)
+    set = predict.to_frame()
+    df = pd.read_csv(io.StringIO(set.to_csv(index=True)))
+    df.rename( columns={'Unnamed: 0':'Date'}, inplace=True )    
+    fig = px.line(df, x='Date', y='predicted_mean', title='Price Graph')
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+  
+    return render_template('index.html', graphJSON=graphJSON)
+
 
 if __name__=='__main__':
     app.run()
